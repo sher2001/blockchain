1ebaed0f51b0257b8e685f2b0c07aa701484d9fb
sher2001 <pvvsharma2000@gmail.com>
2024-11-21 20:38:51 +0000

    Implement locks on blockchain

--- core/blockchain.go
+++ core/blockchain.go
@@ -2,12 +2,14 @@ package core
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/sirupsen/logrus"
 )
 
 type Blockchain struct {
 	store     Storage
+	lock      sync.RWMutex
 	headers   []*Header
 	validator Validator
 }
@@ -37,6 +39,8 @@ func (bc *Blockchain) GetHeader(height uint32) (*Header, error) {
 	if height > bc.Height() {
 		return nil, fmt.Errorf("given height (%d) is too high", height)
 	}
+	bc.lock.Lock()
+	defer bc.lock.Unlock()
 	return bc.headers[height], nil
 }
 
@@ -45,11 +49,16 @@ func (bc *Blockchain) HasBlock(height uint32) bool {
 }
 
 func (bc *Blockchain) Height() uint32 {
+	bc.lock.RLock()
+	defer bc.lock.RUnlock()
 	return uint32(len(bc.headers) - 1)
 }
 
 func (bc *Blockchain) addBlockWithoutValidation(b *Block) error {
+	bc.lock.Lock()
 	bc.headers = append(bc.headers, b.Header)
+	bc.lock.Unlock()
+
 	logrus.WithFields(logrus.Fields{
 		"Height": b.Height,
 		"Hash":   b.Hash(BlockHasher{}),
